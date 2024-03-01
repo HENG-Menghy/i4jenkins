beb6cb5924f6a0d446a00f5f0184ce39e64f0b47
HENG-Menghy <hengmenghy2002@gmail.com>
2024-03-01 15:38:41 +0700

    edit text output in print

--- src/main/java/gic/i4/App.java
+++ src/main/java/gic/i4/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "I4A are too many" );
+        System.out.println( "Hello World! My name is Menghy" );
     }
 }
